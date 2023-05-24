1a161fc4135c900d488b4e7c8d5d75fe1f0c3c0b
Daria Łuniewska <luniewska.d@gmail.com>
2023-05-24 12:10:27 +0200

    recreated page for subscribing for different hubs in react client with react-tostify for notifications

--- signalr-client/src/App.jsx
+++ signalr-client/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import DeathlyHallows from './components/DeathlyHallows/DeathlyHallows'
+import GroupsHouses from './components/GroupsHouses/GroupsHouses'
 import PageViews from './components/PageViews/PageViews'
 
 function App() {
 
   return (
     <>
+      <GroupsHouses />
+      <hr className='defSep'/>
       <DeathlyHallows />
       <hr className='defSep'/>
       <PageViews />
